13b4097d413b2f77f2d0e7f91d68602a4d9dc162
kenatman <76776206+kenatman@users.noreply.github.com>
2021-01-18 22:20:28 +0900

    array

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ alert("I am JS. I am beuatiful.");
 
 const a = 100;
 let b = a - 5;
-a = 3;
+
 console.log(b, a);
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[4]);
