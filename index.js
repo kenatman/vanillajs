df914df5e3fce736f1103a8838a71c0456199556
kenatman <76776206+kenatman@users.noreply.github.com>
2021-01-20 20:48:11 +0900

    event resize

--- index.js
+++ index.js
@@ -1,53 +1,5 @@
-const a = 100;
-let b = a - 5;
-
-console.log(b, a);
-
-const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-console.log(daysOfWeek);
-console.log(daysOfWeek[4]);
-
-const nicoInfo = {
-  name: "Nico",
-  age: 33,
-  favMovie: ["Tenet", "Inception", "Interstella"],
-  favFood: [
-    { name: "Kimchi", taste: "spicy" },
-    { name: "Cheese Burger", taste: "no-spicy" },
-  ],
-};
-
-console.log(nicoInfo.favFood[1].name);
-
-function sayHello(name, age) {
-  return `Hi my name is ${name}  Years of ${age}`;
+function handleResize() {
+  console.log("I have been resized");
 }
 
-const ys = sayHello("Yunsu", 32);
-console.log(ys);
-
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-};
-
-const plus = calculator.plus(3, 7);
-const minus = calculator.minus(3, 7);
-const times = calculator.times(3, 7);
-const divide = calculator.divide(3, 7);
-
-console.log(plus);
-console.log(minus);
-console.log(times);
-console.log(divide);
+window.addEventListener("resize", handleResize);
