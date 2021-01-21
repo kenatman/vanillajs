e26d96336eff00705cb69bcbc05feff1e6d40f35
kenatman <76776206+kenatman@users.noreply.github.com>
2021-01-21 21:47:22 +0900

    function what the ff

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const age = prompt("How old are you???");
+const title = document.querySelector("#title");
 
-if (age <= 19) {
-  console.log("Go home");
-} else if (age > 19 && age < 30) {
-  console.log("Enjoy");
-} else {
-  console.log("Too old");
+const BASE_COLOR = "rgb(52,73,94)";
+const OTHER_COLOR = "red";
+
+function handClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handClick);
+}
+init();
