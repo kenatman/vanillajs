173930c22023c801e980a9287be555c39813d7f6
kenatman <76776206+kenatman@users.noreply.github.com>
2021-02-10 22:27:18 +0900

    challenge 1

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const title = document.querySelector("#title");
+// <⚠️ DONT DELETE THIS ⚠️>
 
-const CLICKED_CLASS = "clicked";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
 
-function handClick() {
-  title.classList.toggle(CLICKED_CLASS);
-}
+/*
+✅ The text of the title should change when the mouse is on top of it.
+✅ The text of the title should change when the mouse is leaves it.
+✅ When the window is resized the title should change.
+✅ On right click the title should also change.
+✅ The colors of the title should come from a color from the colors array.
+✅ DO NOT CHANGE .css, or .html files.
+✅ ALL function handlers should be INSIDE of "superEventHandler"
+*/
 
-function init() {
-  title.addEventListener("click", handClick);
-}
-init();
+const h2 = document.querySelector("h2");
+
+const superEventHandler = {
+  mouseOn: () => {
+    (h2.innerText = `Your mouse is here!`), (h2.style.color = colors[0]);
+  },
+  mouseLeave: () => {
+    (h2.innerText = `Your mouse is gone!!!`), (h2.style.color = colors[1]);
+  },
+  resize: () => {
+    (h2.innerText = `You just resized`), (h2.style.color = colors[2]);
+  },
+  CM: () => {
+    (h2.innerText = `This is a right click`), (h2.style.color = colors[4]);
+  },
+};
+h2.addEventListener("mouseenter", superEventHandler.mouseOn);
+h2.addEventListener("mouseleave", superEventHandler.mouseLeave);
+window.addEventListener("resize", superEventHandler.resize);
+window.addEventListener("contextmenu", superEventHandler.CM);
