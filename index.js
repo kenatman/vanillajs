a3fbbdaf17c3fbd7973c714fbdd6f5ea1c0ad7ee
kenatman <76776206+kenatman@users.noreply.github.com>
2021-02-16 07:25:54 +0900

    challenge 3 ans(""in"")

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const select = document.querySelector(".js-select");
 
-const COUNTRY = "country";
-let currentCT = localStorage.getItem(COUNTRY);
-const options = select.querySelectorAll("option");
-
-const option1 = options[1];
-const option2 = options[2];
-const option3 = options[3];
-const option4 = options[4];
-
-function saveCountry(event) {
-  const savedValue = event.target.value;
-
-  localStorage.setItem(COUNTRY, savedValue);
+function handleChange() {
+  const selected = select.value;
+  localStorage.setItem("country", selected);
 }
 
-select.addEventListener("change", saveCountry);
-
-if (currentCT !== null) {
-  if (option1.value === currentCT) {
-    option1.setAttribute("selected", "");
-  } else if (option2.value === currentCT) {
-    option2.setAttribute("selected", "");
-  } else if (option3.value === currentCT) {
-    option3.setAttribute("selected", "");
-  } else if (option4.value === currentCT) {
-    option4.setAttribute("selected", "");
+function loadCountries() {
+  const selected = localStorage.getItem("country");
+  if (selected) {
+    const option = document.querySelector(`option[value="${selected}"]`);
+    option.selected = true;
   }
 }
+
+loadCountries();
+select.addEventListener("change", handleChange);
